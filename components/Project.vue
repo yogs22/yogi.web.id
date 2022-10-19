f383809a579ccc5745e511b049255bbd0cb98cd9
Yogi Prasetyawan <yogi@corenglish.com>
2022-10-19 10:39:50 +0700

    Add carousel effect

--- components/Project.vue
+++ components/Project.vue
@@ -1,45 +1,57 @@
-<!-- Please remove this file from your project -->
-<template>
+<template> <!-- important to add template-->
   <section id="project" class="bg-primary text-white">
     <div class="container">
       <div class="text-center mb-4">
         <h2 class="mb-2">Take a look at my recent projects</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </div>
-      <div class="row mt-5">
-        <div class="col-md-4 mb-5">
-          <div class="project">
-            <div class="project-header">
-              <img src="~/static/img/projects/dparagon.png" class="w-100" alt="dparagon project">
-            </div>
-            <div class="project-body">
-              <strong class="d-block text-lg mb-1">D'Paragon</strong>
-              <a href="#" class="text-sm">See Project Detail</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4 mb-5">
-          <div class="project">
-            <div class="project-header">
-              <img src="~/static/img/projects/djurkam.png" class="w-100" alt="djurkam project">
-            </div>
-            <div class="project-body">
-              <strong class="d-block text-lg mb-2">Djuragan Kamar</strong>
-              <a href="#" class="text-sm">See Project Detail</a>
+      <div class="w-100 mt-5">
+        <carousel :loop="true" :nav="true" :margin="25" :responsive="{0:{items:1},461:{items:2},821:{items:3}}">
+          <div class="w-100 mb-5">
+            <div class="project">
+              <div class="project-header">
+                <img src="~/static/img/projects/dparagon.png" class="w-100" alt="dparagon project">
+              </div>
+              <div class="project-body">
+                <strong class="d-block text-lg mb-1">D'Paragon</strong>
+                <a href="#" class="text-sm">See Project Detail</a>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="col-md-4 mb-5">
-          <div class="project">
-            <div class="project-header">
-              <img src="~/static/img/projects/djurvoc.png" class="w-100" alt="djurvoc project">
-            </div>
-            <div class="project-body">
-              <strong class="d-block text-lg mb-2">Djuragan Voucher</strong>
-              <a href="#" class="text-sm">See Project Detail</a>
+          <div class="w-100 mb-5">
+            <div class="project">
+              <div class="project-header">
+                <img src="~/static/img/projects/djurkam.png" class="w-100" alt="djurkam project">
+              </div>
+              <div class="project-body">
+                <strong class="d-block text-lg mb-2">Djuragan Kamar</strong>
+                <a href="#" class="text-sm">See Project Detail</a>
+              </div>
             </div>
           </div>
-        </div>
+          <div class="w-100 mb-5">
+            <div class="project">
+              <div class="project-header">
+                <img src="~/static/img/projects/vexagame.png" class="w-100" alt="vexagame project">
+              </div>
+              <div class="project-body">
+                <strong class="d-block text-lg mb-2">Vexa Game</strong>
+                <a href="#" class="text-sm">See Project Detail</a>
+              </div>
+            </div>
+          </div>
+          <div class="w-100 mb-5">
+            <div class="project">
+              <div class="project-header">
+                <img src="~/static/img/projects/djurvoc.png" class="w-100" alt="djurvoc project">
+              </div>
+              <div class="project-body">
+                <strong class="d-block text-lg mb-2">Djuragan Voucher</strong>
+                <a href="#" class="text-sm">See Project Detail</a>
+              </div>
+            </div>
+          </div>
+        </carousel>
       </div>
     </div>
   </section>
--- components/Testimoni.vue
+++ components/Testimoni.vue
@@ -7,13 +7,38 @@
         <span class="text-muted d-block mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</span>
         <span class="text-muted d-block">tempor incididunt ut labore et dolore magna aliqua.</span>
       </div>
-      <div class="p-0 pt-5 p-md-5">
-        <div class="container-fluid">
-          <p class="text-muted">“It's been a pleasure working with Yogi Prasetyawan as a Development Team for my half a year. The task that we work together is develop and improve web and mobile applications for reservations by users in the D’Paragon & Djuragan Kamar Company. Yogi has been working diligently as a Backend Developer at both company aboutproximate for almost 1.5 years. Welldone Yogi!”</p>
-          <img src="~/static/img/testi/fauzi.jpg" alt="fauzi" class="img-rounded mt-4">
-          <p class="text-lg"><strong>Fauzi Zaki</strong></p>
+      <carousel :loop="true" :items="1" :nav="false" :autoplay="true">
+        <div class="p-0 pt-5 px-md-5">
+          <div class="container-fluid">
+            <div class="px-md-5">
+              <p class="text-muted">“It's been a pleasure working with Yogi Prasetyawan as a Development Team for my half a year. The task that we work together is develop and improve web and mobile applications for reservations by users in the D’Paragon & Djuragan Kamar Company. Yogi has been working diligently as a Backend Developer at both company aboutproximate for almost 1.5 years. Welldone Yogi!”</p>
+            </div>
+            <img src="~/static/img/testi/fauzi.jpg" alt="fauzi" class="img-rounded testi-img my-4">
+            <p class="text-lg m-0"><strong>Fauzi Zaki</strong></p>
+            <p class="m-0 text-sm text-muted">Software Engineer at Telkom Indonesia</p>
+          </div>
         </div>
-      </div>
+        <div class="p-0 pt-5 px-md-5">
+          <div class="container-fluid">
+            <div class="px-md-5">
+              <p class="text-muted">“Yogi's work is very well done. With his help, i can integrate chatbot whatsapp with API backend that ease me to get response very quick”</p>
+            </div>
+            <img src="~/static/img/testi/amirul.jpg" alt="amirul" class="img-rounded testi-img my-4">
+            <p class="text-lg m-0"><strong>Muhammad Amirul M.</strong></p>
+            <p class="m-0 text-sm text-muted">Odoo Consultant at Arkana Solusi Digital</p>
+          </div>
+        </div>
+        <div class="p-0 pt-5 px-md-5">
+          <div class="container-fluid">
+            <div class="px-md-5">
+              <p class="text-muted">“Yogi is a great frontend developer, he has neat and good coding skills. I really admire his hard work on the team. and in my opinion, you are the team that inspired me the most”</p>
+            </div>
+            <img src="~/static/img/testi/zaki.jpg" alt="zaki" class="img-rounded testi-img my-4">
+            <p class="text-lg m-0"><strong>Zaki Maliki</strong></p>
+            <p class="m-0 text-sm text-muted">Academic Coding Instructor</p>
+          </div>
+        </div>
+      </carousel>
     </div>
   </section>
 </template>
